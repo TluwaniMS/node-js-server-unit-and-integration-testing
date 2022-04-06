c046fd16af2a1010f0320dca36ed43c842761610
tluwaniMS <tluwani2020@gmail.com>
2022-04-06 15:35:22 +0200

    adding-route-configs

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const app = express();
-
+const studentsroute = require("./routes/students-route");
+const schoolsRoute = require("./routes/schools-route");
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
+app.use("/api/students", studentsroute);
+app.use("/api/schools", schoolsRoute);
 
 module.exports = {
   app
