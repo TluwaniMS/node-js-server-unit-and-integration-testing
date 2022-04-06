38a7f61772f383980b8bd9c4f83245adcb5f8c88
tluwaniMS <tluwani2020@gmail.com>
2022-04-06 14:34:58 +0200

    adding-database-queries-skeleton-functions

---
+++ database-queries/SchoolDBQueries.js
@@ -0,0 +1,15 @@
+const { SchoolModel } = require("../database-models/School");
+
+const createSchool = async () => {};
+
+const createMultipleSchools = async () => {};
+
+const getAllSchools = async () => {};
+
+const getSchoolById = async () => {};
+
+const updateSchoolInformation = async () => {};
+
+const deleteSchool = async () => {};
+
+module.exports = {};
---
+++ database-queries/StudentDBQueries.js
@@ -0,0 +1,15 @@
+const { StudentModel } = require("../database-models/Student");
+
+const createStudent = async () => {};
+
+const createMultipleStudents = async () => {};
+
+const getAllStudents = async () => {};
+
+const getStudentById = async () => {};
+
+const updateStudentInformation = async () => {};
+
+const deleteStudent = async () => {};
+
+module.exports = {};
