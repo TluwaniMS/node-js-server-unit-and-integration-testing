2cfbe44251d2cc5109061ce66ed5a860beeae796
tluwaniMS <tluwani2020@gmail.com>
2022-04-07 10:40:51 +0200

    adding-logic-to-student-db-queries

--- database-queries/StudentDBQueries.js
+++ database-queries/StudentDBQueries.js
@@ -1,15 +1,56 @@
 const { StudentModel } = require("../database-models/Student");
 
-const createStudent = async () => {};
+const createStudent = async (studentInformation) => {
+  await StudentModel.create({
+    name: studentInformation.name,
+    surname: studentInformation.surname,
+    gender: studentInformation.gender,
+    grade: studentInformation.grade,
+    dateOfBirth: studentInformation.dateOfBirth,
+    sports: studentInformation.sports
+  });
 
-const createMultipleStudents = async () => {};
+  return `Operation completed successfully.`;
+};
 
-const getAllStudents = async () => {};
+const getAllStudents = async () => {
+  const students = await StudentModel.find({});
 
-const getStudentById = async () => {};
+  return students;
+};
 
-const updateStudentInformation = async () => {};
+const getStudentById = async (studentId) => {
+  const student = await StudentModel.findOne({ _id: studentId });
 
-const deleteStudent = async () => {};
+  return student;
+};
 
-module.exports = {};
+const updateStudentInformationById = async (studentId, studentInformation) => {
+  await StudentModel.updateOne(
+    { _id: studentId },
+    {
+      name: studentInformation.name,
+      surname: studentInformation.surname,
+      gender: studentInformation.gender,
+      grade: studentInformation.grade,
+      dateOfBirth: studentInformation.dateOfBirth,
+      sports: studentInformation.sports
+    }
+  );
+
+  return `Operation completed successfully.`;
+};
+
+const deleteStudentById = async (studentId) => {
+  await StudentModel.deleteOne({ _id: studentId });
+
+  return `Operation completed successfully.`;
+};
+
+module.exports = {
+  createStudent,
+  getAllStudents,
+  getStudentById,
+  updateStudentInformationById,
+  deleteStudentById
+};
