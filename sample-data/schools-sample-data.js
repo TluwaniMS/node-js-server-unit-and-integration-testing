26e38079ac180c7e288162df8d3af66472ebc86a
tluwaniMS <tluwani2020@gmail.com>
2022-04-07 13:36:20 +0200

    adding-first-phase-of-students-sample-data

---
+++ sample-data/schools-sample-data.js
@@ -0,0 +1,42 @@
+const Schools = [
+  {
+    _id: "",
+    name: "Kgwadu Primary School",
+    district: "Capricorn District Municipality	",
+    level: "Primary",
+    students: []
+  },
+  {
+    _id: "",
+    name: "Marobathota  High School",
+    district: "Mopani District Municipality	",
+    level: "Secondary",
+    students: []
+  },
+  {
+    _id: "",
+    name: "Mamotshana Primary School",
+    district: "Sekhukhune District Municipality	",
+    level: "Primary",
+    students: []
+  },
+  {
+    _id: "",
+    name: "Tom Naude Technical High School",
+    district: "Mopani District Municipality	",
+    level: "Secondary",
+    students: []
+  },
+  {
+    _id: "",
+    name: "Dendron High School",
+    district: "Sekhukhune District Municipality	",
+    level: "Secondary",
+    students: []
+  },
+  { _id: "", name: "Mbilwi High School", district: "Vhembe District Municipality", level: "Secondary", students: [] }
+];
+
+module.exports = {
+  Schools
+};
---
+++ sample-data/students-sample-data.js
@@ -0,0 +1,227 @@
+const Students = [
+  {
+    _id: "",
+    name: "Dean",
+    surname: "Hill",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Lufuno",
+    surname: "Sekgothe",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Refilwe",
+    surname: "Phago",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Seba",
+    surname: "Sibika",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Katlego",
+    surname: "Mamani",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Idah",
+    surname: "Leseme",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Jessica",
+    surname: "Shadung",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Tshego",
+    surname: "Seoka",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Jethro",
+    surname: "Mokgohloa",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Kgaogelo",
+    surname: "Munyangane",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Mmadi",
+    surname: "Ndlovu",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Abigail",
+    surname: "Sekhosana",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Mmakosha",
+    surname: "Deagrela",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Dorcus",
+    surname: "Maruma",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Denzel",
+    surname: "Mophosho",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Lerato",
+    surname: "Sehume",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Londokuhle",
+    surname: "Ndala",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Thovhedzo",
+    surname: "Maosa",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Reneilwe",
+    surname: "Dikana",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Philicia",
+    surname: "Sothobe",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Female"
+  },
+  {
+    _id: "",
+    name: "Eddy",
+    surname: "Manaka",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Mathibela",
+    surname: "Serumula",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Mpho",
+    surname: "Leshabane",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Lehlohonolo",
+    surname: "Sebata",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  },
+  {
+    _id: "",
+    name: "Mpumi",
+    surname: "Khoza",
+    grade: "",
+    dateOfBirth: "",
+    sports: [],
+    gender: "Male"
+  }
+];
