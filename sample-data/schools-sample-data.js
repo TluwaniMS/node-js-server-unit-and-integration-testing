5d0bb4b265eb194d4157a94084ec2c902da353dc
tluwaniMS <tluwani2020@gmail.com>
2022-04-07 19:02:45 +0200

    adding-students-to-schools-sample-data

--- sample-data/schools-sample-data.js
+++ sample-data/schools-sample-data.js
@@ -4,42 +4,70 @@ const Schools = [
     name: "Kgwadu Primary School",
     district: "Capricorn District Municipality	",
     level: "Primary",
-    students: []
+    students: [
+      "624edb425712f22ee59fd988",
+      "624edb425712f22ee59fd98d",
+      "624edb425712f22ee59fd992",
+      "624edb425712f22ee59fd995",
+      "624edb425712f22ee59fd997"
+    ]
   },
   {
     _id: "624eddc76022740b286b6be2",
     name: "Marobathota  High School",
     district: "Mopani District Municipality	",
     level: "Secondary",
-    students: []
+    students: [
+      "624edb425712f22ee59fd987",
+      "624edb425712f22ee59fd993",
+      "624edb425712f22ee59fd998",
+      "624edb425712f22ee59fd99d",
+      "624edb425712f22ee59fd99e",
+      "624edb425712f22ee59fd99f"
+    ]
   },
   {
     _id: "624eddc76022740b286b6be3",
     name: "Mamotshana Primary School",
     district: "Sekhukhune District Municipality	",
     level: "Primary",
-    students: []
+    students: [
+      "624edb425712f22ee59fd98a",
+      "624edb425712f22ee59fd98f",
+      "624edb425712f22ee59fd994",
+      "624edb425712f22ee59fd996"
+    ]
   },
   {
     _id: "624eddc76022740b286b6be4",
     name: "Tom Naude Technical High School",
     district: "Mopani District Municipality	",
     level: "Secondary",
-    students: []
+    students: [
+      "624edb425712f22ee59fd989",
+      "624edb425712f22ee59fd991",
+      "624edb425712f22ee59fd999",
+      "624edb425712f22ee59fd99c"
+    ]
   },
   {
     _id: "624eddc76022740b286b6be5",
     name: "Dendron High School",
     district: "Sekhukhune District Municipality	",
     level: "Secondary",
-    students: []
+    students: [
+      "624edb425712f22ee59fd98b",
+      "624edb425712f22ee59fd990",
+      "624edb425712f22ee59fd99a",
+      "624edb425712f22ee59fd99b"
+    ]
   },
   {
     _id: "624eddc76022740b286b6be6",
     name: "Mbilwi High School",
     district: "Vhembe District Municipality",
     level: "Secondary",
-    students: []
+    students: ["624edb425712f22ee59fd98c", "624edb425712f22ee59fd98e"]
   }
 ];
 
