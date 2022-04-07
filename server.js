ab8fbb0de5cf777163b77318dec580c00f9e5a8a
tluwaniMS <tluwani2020@gmail.com>
2022-04-07 19:50:50 +0200

    adding-final-touches-to-server-config

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { app } = require("./app");
 const { connectToDataBase } = require("./database-configuration");
+const { getAllStudents } = require("./database-queries/StudentDBQueries");
+const { populateDataBaseWithSampleData } = require("./main-migration-script");
 
 const PORT = process.env.PORT | 5000;
 
@@ -9,4 +11,7 @@ app.listen(PORT, () => {
 
 (async () => {
   await connectToDataBase();
+  const students = await getAllStudents();
+
+  students.length === 0 ? await populateDataBaseWithSampleData() : console.log(`database has already been populated`);
 })();
