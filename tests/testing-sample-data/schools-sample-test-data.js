bb9e7669cacd35f2b31d8f82bb7b3345c511647a
tluwaniMS <tluwani2020@gmail.com>
2022-04-09 12:42:30 +0200

    adding-test-sample-data

---
+++ tests/testing-sample-data/schools-sample-test-data.js
@@ -0,0 +1,18 @@
+const Schools = [
+  {
+    _id: "624eddc76022740b286b6be1",
+    name: "Kgwadu Primary School",
+    district: "Capricorn District Municipality	",
+    level: "Primary",
+    students: ["624edb425712f22ee59fd988", "624edb425712f22ee59fd98a"]
+  },
+  {
+    _id: "624eddc76022740b286b6be2",
+    name: "Marobathota  High School",
+    district: "Mopani District Municipality	",
+    level: "Secondary",
+    students: ["624edb425712f22ee59fd987", "624edb425712f22ee59fd989", "624edb425712f22ee59fd98b"]
+  }
+];
+
+module.exports = { Schools };
---
+++ tests/testing-sample-data/students-sample-test-data.js
@@ -0,0 +1,49 @@
+const Students = [
+  {
+    _id: "624edb425712f22ee59fd987",
+    name: "Dean",
+    surname: "Hill",
+    grade: 11,
+    age: 17,
+    sports: ["Swimming", "Rugby"],
+    gender: "Male"
+  },
+  {
+    _id: "624edb425712f22ee59fd988",
+    name: "Lufuno",
+    surname: "Sekgothe",
+    grade: 6,
+    age: 12,
+    sports: ["Netball", "Swimming"],
+    gender: "Female"
+  },
+  {
+    _id: "624edb425712f22ee59fd989",
+    name: "Refilwe",
+    surname: "Phago",
+    grade: 9,
+    age: 15,
+    sports: ["Soccer"],
+    gender: "Female"
+  },
+  {
+    _id: "624edb425712f22ee59fd98a",
+    name: "Seba",
+    surname: "Sibika",
+    grade: 5,
+    age: 11,
+    sports: ["Soccer", "Rugby"],
+    gender: "Female"
+  },
+  {
+    _id: "624edb425712f22ee59fd98b",
+    name: "Katlego",
+    surname: "Mamani",
+    grade: 10,
+    age: 16,
+    sports: ["Netball", "Swimming"],
+    gender: "Female"
+  }
+];
+
+module.exports = { Students };
