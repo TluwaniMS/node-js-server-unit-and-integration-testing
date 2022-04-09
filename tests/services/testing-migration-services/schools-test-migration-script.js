d7278881e60282ee27411611640a3a1a1cf5ac3f
tluwaniMS <tluwani2020@gmail.com>
2022-04-09 12:44:53 +0200

    adding-final-touches-to-test-migration-scripts

--- tests/services/testing-migration-services/schools-test-migration-script.js
+++ tests/services/testing-migration-services/schools-test-migration-script.js
@@ -1,7 +1,8 @@
 const { SchoolModel } = require("../../../database-models/School");
+const { Schools } = require("../../testing-sample-data/schools-sample-test-data");
 
 const createTestSchoolsSampleData = async () => {
-  await SchoolModel.insertMany();
+  await SchoolModel.insertMany(Schools);
 
   console.log(`The test students migration script has run successfuly.`);
 };
--- tests/services/testing-migration-services/students-test-migration-script.js
+++ tests/services/testing-migration-services/students-test-migration-script.js
@@ -1,7 +1,8 @@
 const { StudentModel } = require("../../../database-models/Student");
+const { Students } = require("../../testing-sample-data/students-sample-test-data");
 
 const createTestStudentsSampleData = async () => {
-  await StudentModel.insertMany();
+  await StudentModel.insertMany(Students);
 
   console.log(`The test schools migration script has run successfuly.`);
 };
