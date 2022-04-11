72d4ffcc56bd585f40cdd76f52997162ceee8d6d
tluwaniMS <tluwani2020@gmail.com>
2022-04-11 09:56:01 +0200

    adding-sample-update-data

--- tests/testing-sample-data/sample-data-testing-school-queries.js
+++ tests/testing-sample-data/sample-data-testing-school-queries.js
@@ -4,4 +4,16 @@ const sampleSchool = {
   level: "Secondary"
 };
 
-module.exports = { sampleSchool };
+const sampleSchoolForUpdateTests = {
+  _id: "624eddc76022740b286b6be2",
+  data: {
+    name: "Moria High School",
+    district: "Mopani District Municipality	",
+    level: "Secondary",
+    students: ["624edb425712f22ee59fd987", "624edb425712f22ee59fd989", "624edb425712f22ee59fd98b"]
+  }
+};
+
+const sampleSchoolUpdatedName = "Moria High School";
+
+module.exports = { sampleSchool, sampleSchoolForUpdateTests, sampleSchoolUpdatedName };
--- tests/testing-sample-data/sample-data-testing-student-queries.js
+++ tests/testing-sample-data/sample-data-testing-student-queries.js
@@ -5,4 +5,18 @@ const sampleStudent = {
   sports: ["Soccer"]
 };
 
-module.exports = { sampleStudent };
+const sampleStudentForUpdateTest = {
+  _id: "624edb425712f22ee59fd988",
+  data: {
+    name: "Lufuno",
+    surname: "Marimba",
+    grade: 6,
+    age: 12,
+    sports: ["Netball", "Swimming"],
+    gender: "Female"
+  }
+};
+
+const sampleStudentUpdatedSurname = "Marimba";
+
+module.exports = { sampleStudent, sampleStudentForUpdateTest, sampleStudentUpdatedSurname };
