700859c4f57399bb169a9c8c54f05bbc9af740a9
tluwaniMS <tluwani2020@gmail.com>
2022-04-12 14:29:22 +0200

    adding-error-handling-middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const app = express();
+const { unknownRequests, errorResponse } = require("./middleware/errorHandler");
 const studentsroute = require("./routes/students-route");
 const schoolsRoute = require("./routes/schools-route");
 dotenv.config({ path: "./.env" });
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 app.use("/api/students", studentsroute);
 app.use("/api/schools", schoolsRoute);
+app.use(unknownRequests);
+app.use(errorResponse);
 
 module.exports = {
   app
---
+++ middleware/errorhandler.js
@@ -0,0 +1,27 @@
+const errorHandler = (handler) => {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+const unknownRequests = (req, res, next) => {
+  const error = new Error("Route requested not found");
+  error.status = 404;
+  next(error);
+};
+
+const errorResponse = (error, req, res, next) => {
+  if (error.status) {
+    console.log(error);
+    res.status(error.status).send({ message: `${error.message}... :(` });
+  } else {
+    console.log(error);
+    res.status(500).send({ message: "Internal server error, please try again later... :(" });
+  }
+};
+
+module.exports = { errorHandler, unknownRequests, errorResponse };
