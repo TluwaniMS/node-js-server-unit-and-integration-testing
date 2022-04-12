2cf65aa0daa4f948fc957082a103bfae9e11e58c
tluwaniMS <tluwani2020@gmail.com>
2022-04-12 10:29:48 +0200

    adding-first-phase-of-end-point-testing-logic

--- tests/integration-testing/end-point-tests/schools-end-point-testing.js
+++ tests/integration-testing/end-point-tests/schools-end-point-testing.js
@@ -5,30 +5,40 @@ module.exports = () =>
   describe("Testing the schools end-points", () => {
     describe("Testing the create school end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/schools/create-school");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the get all schools end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/schools/get-all-schools");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the get school by id end-point", () => {
       it("It should return a status 200 response", async () => {
+        const response = request(app).get("/api/schools/get-school-by-id");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the update school information by id end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/schools/update-school-information-by-id");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the delete school by id end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/schools/delete-school-by-id");
+        
         expect(1).toEqual(1);
       });
     });
--- tests/integration-testing/end-point-tests/students-end-point-testing.js
+++ tests/integration-testing/end-point-tests/students-end-point-testing.js
@@ -5,30 +5,40 @@ module.exports = () =>
   describe("Testing the students end-points", () => {
     describe("Testing the create student end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/students/create-student");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the get all students end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/students/get-all-students");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the get student by id end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/students/get-student-by-id/");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the update student information by id end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/students/update-student-information-by-id");
+
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the delete student by id end-point", () => {
       it("", async () => {
+        const response = request(app).get("/api/students/delete-student-by-id");
+        
         expect(1).toEqual(1);
       });
     });
