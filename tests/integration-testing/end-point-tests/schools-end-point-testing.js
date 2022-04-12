6da66a3a5205e9ae8bcf04d2a288ad8cbb2263b2
tluwaniMS <tluwani2020@gmail.com>
2022-04-12 11:10:03 +0200

    adding-get-all-end-point-query-testing-logic

--- tests/integration-testing/end-point-tests/schools-end-point-testing.js
+++ tests/integration-testing/end-point-tests/schools-end-point-testing.js
@@ -1,44 +1,56 @@
 const { app } = require("../../../app");
 const request = require("supertest");
+const {
+  sampleSchool,
+  sampleSchoolToBeUsedForByIdQueries,
+  sampleSchoolForUpdateTests,
+  sampleSchoolUpdatedName
+} = require("../../testing-sample-data/sample-data-testing-school-queries");
 
 module.exports = () =>
   describe("Testing the schools end-points", () => {
     describe("Testing the create school end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/schools/create-school");
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/schools/create-school");
 
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the get all schools end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/schools/get-all-schools");
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/schools/get-all-schools");
 
-        expect(1).toEqual(1);
+        expect(response.status).toEqual(200);
+      });
+
+      it("It should return an array with 2 schools", async () => {
+        const response = await request(app).get("/api/schools/get-all-schools");
+
+        expect(response.body.data).toHaveLength(2);
       });
     });
 
     describe("Testing the get school by id end-point", () => {
       it("It should return a status 200 response", async () => {
-        const response = request(app).get("/api/schools/get-school-by-id");
+        const response = await request(app).get("/api/schools/get-school-by-id");
 
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the update school information by id end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/schools/update-school-information-by-id");
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/schools/update-school-information-by-id");
 
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the delete school by id end-point", () => {
-      it("", async () => {
+      it("It should return a status 200 response", async () => {
         const response = request(app).get("/api/schools/delete-school-by-id");
-        
+
         expect(1).toEqual(1);
       });
     });
--- tests/integration-testing/end-point-tests/students-end-point-testing.js
+++ tests/integration-testing/end-point-tests/students-end-point-testing.js
@@ -1,44 +1,56 @@
 const { app } = require("../../../app");
 const request = require("supertest");
+const {
+  sampleStudent,
+  sampleStudentForUpdateTest,
+  sampleStudentToBeUsedForByIdQueries,
+  sampleStudentUpdatedSurname
+} = require("../../testing-sample-data/sample-data-testing-student-queries");
 
 module.exports = () =>
   describe("Testing the students end-points", () => {
     describe("Testing the create student end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/students/create-student");
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/students/create-student");
 
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the get all students end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/students/get-all-students");
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/students/get-all-students");
 
-        expect(1).toEqual(1);
+        expect(response.status).toEqual(200);
+      });
+
+      it("It should return an array with 6 students", async () => {
+        const response = await request(app).get("/api/students/get-all-students");
+
+        expect(response.body.data).toHaveLength(5);
       });
     });
 
     describe("Testing the get student by id end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/students/get-student-by-id/");
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/students/get-student-by-id/");
 
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the update student information by id end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/students/update-student-information-by-id");
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/students/update-student-information-by-id");
 
         expect(1).toEqual(1);
       });
     });
 
     describe("Testing the delete student by id end-point", () => {
-      it("", async () => {
-        const response = request(app).get("/api/students/delete-student-by-id");
-        
+      it("It should return a status 200 response", async () => {
+        const response = await request(app).get("/api/students/delete-student-by-id");
+
         expect(1).toEqual(1);
       });
     });
