bbb9e5e131e4f620e216202bfd06aaa845548f3f
tluwaniMS <tluwani2020@gmail.com>
2022-04-14 15:54:38 +0200

    adding-student-detailed-information-object-matcher-test

--- tests/unit-testing/business-logic-testing/students-logic-testing.test.js
+++ tests/unit-testing/business-logic-testing/students-logic-testing.test.js
@@ -2,9 +2,16 @@ const {
   returnRequiredStudentFields,
   addTotalSportsPlayedFieldToStudentObject
 } = require("../../../services/student-services");
+const {
+  detailedStudentInformationObjectMatcher
+} = require("../../testing-object-matchers/students-object-property-matchers");
+const {
+  sampleStudentTobeUsedFoBusinessLogicUnitTests
+} = require("../../testing-sample-data/sample-student-data-for-testing-business-logic");
 
 describe("Testing student services business logic", () => {
-  it("", () => {
-    expect(1).toEqual(1);
+  it("It should return an object that matches the specified object", () => {
+    const requiredStudentFields = returnRequiredStudentFields(sampleStudentTobeUsedFoBusinessLogicUnitTests);
+    expect(requiredStudentFields).toEqual(expect.objectContaining(detailedStudentInformationObjectMatcher));
   });
 });
