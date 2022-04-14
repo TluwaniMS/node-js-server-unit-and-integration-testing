31c16bd6716fb6bf68ea3b5173189b71441daaf0
tluwaniMS <tluwani2020@gmail.com>
2022-04-14 13:36:49 +0200

    adding-services-to-schools

--- services/school-services.js
+++ services/school-services.js
@@ -1,1 +1,15 @@
+const calculateTotalStudentsInschool = (studentsArray) => {
+  return studentsArray.length;
+};
 
+const returnRequiredSchoolFields = ({ _id, name, district, level }) => {
+  return { _id, name, district, level };
+};
+
+const addTotalStudentsFieldToSchoolObject = (schoolObject, totalStudents) => {
+  schoolObject.totalStudents = totalStudents;
+
+  return schoolObject;
+};
+
+module.exports = { calculateTotalStudentsInschool, returnRequiredSchoolFields, addTotalStudentsFieldToSchoolObject };
