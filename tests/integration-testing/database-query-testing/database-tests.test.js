e53936899b6c1c66fc759939060e8e13747c6c66
tluwaniMS <tluwani2020@gmail.com>
2022-04-15 11:00:21 +0200

    adding-tests-for-database-queries

--- tests/integration-testing/database-query-testing/database-tests.test.js
+++ tests/integration-testing/database-query-testing/database-tests.test.js
@@ -1,7 +1,9 @@
 const { runSetupAndTearDownscripts } = require("../../services/testing-auxiliary-services/set-up-and-tear-down-logic");
 const schoolTests = require("../database-query-tests/schools-database-query-testing");
 const studentTests = require("../database-query-tests/students-database-query-testing");
+const testServiceTests = require("../database-query-tests/test-service-tests");
 
 runSetupAndTearDownscripts();
 schoolTests();
 studentTests();
+testServiceTests();
---
+++ tests/integration-testing/database-query-tests/test-service-tests.js
@@ -0,0 +1,63 @@
+const { createSchool } = require("../../../database-queries/SchoolDBQueries");
+const { createStudent } = require("../../../database-queries/StudentDBQueries");
+const {
+  getSchoolByName,
+  deleteSchoolByName
+} = require("../../services/testing-test-services-for-database-queries/schools-test-database-queries-services");
+const {
+  getStudentByName,
+  deleteStudentByName
+} = require("../../services/testing-test-services-for-database-queries/student-test-database-queries-services");
+const { sampleSchool } = require("../../testing-sample-data/sample-data-testing-school-queries");
+const { sampleStudent } = require("../../testing-sample-data/sample-data-testing-student-queries");
+
+module.exports = () =>
+  describe("Testing test service data-base queries", () => {
+    describe("Testing school testing auxiliary database queries", () => {
+      beforeAll(async () => {
+        await createSchool(sampleSchool);
+      });
+
+      afterAll(async () => {
+        await deleteSchoolByName(sampleSchool.name);
+      });
+
+      it("It should return an object that matches the specified object", async () => {
+        const school = await getSchoolByName(sampleSchool.name);
+
+        expect(school).toHaveProperty("district", sampleSchool.district);
+      });
+
+      it("It should return null", async () => {
+        await deleteSchoolByName(sampleSchool.name);
+
+        const school = await getSchoolByName(sampleSchool.name);
+
+        expect(school).toEqual(null);
+      });
+    });
+
+    describe("Testing student testing auxiliary database queries", () => {
+      beforeAll(async () => {
+        await createStudent(sampleStudent);
+      });
+
+      afterAll(async () => {
+        await deleteStudentByName(sampleStudent.name);
+      });
+
+      it("It should return an object that matches the specified object", async () => {
+        const student = await getStudentByName(sampleStudent.name);
+
+        expect(student).toHaveProperty("surname", sampleStudent.surname);
+      });
+
+      it("It should return null", async () => {
+        await deleteStudentByName(sampleStudent.name);
+
+        const student = await getStudentByName(sampleStudent.name);
+
+        expect(student).toEqual(null);
+      });
+    });
+  });
